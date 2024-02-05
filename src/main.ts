c8bc536bb3118e963cb060e57b2afe42c55a36fe
ggulaman <gulamman@protonmail.com>
2024-02-05 19:13:46 +0100

    adding app origins 2

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors({
-    origin: ['http://localhost:3001', 'http://localhost:3001', 'https://h2evolution.netlify.app/'], // Replace with the origin of your React app
+    origin: ['http://localhost:3001', 'http://localhost:3001', 'https://h2evolution.netlify.app'], // Replace with the origin of your React app
     methods: 'GET',
     credentials: true,
   }));
